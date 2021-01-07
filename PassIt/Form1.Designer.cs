10fbaf298e5796f5f60153009e824654afedf201
sapirnoam <42315483+sapirnoam@users.noreply.github.com>
2021-01-07 15:20:03 +0200

    Add project files.

---
+++ PassIt/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace PassIt
+{
+    partial class PassItVPN
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PassItVPN));
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ConnectButton
+            // 
+            resources.ApplyResources(this.ConnectButton, "ConnectButton");
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.connectButton);
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            resources.ApplyResources(this.progressBar1, "progressBar1");
+            this.progressBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Value = 1;
+            // 
+            // PassItVPN
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ConnectButton);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "PassItVPN";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ PassIt/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Security.Principal;
+using System.Security.Claims;
+
+namespace PassIt
+{
+    public partial class PassItVPN : Form
+    {
+        private bool IsAdmin
+        {
+            get
+            {
+                WindowsIdentity identity = WindowsIdentity.GetCurrent();
+                if (identity != null)
+                {
+                    WindowsPrincipal principal = new WindowsPrincipal(identity);
+                    List<Claim> list = new List<Claim>(principal.UserClaims);
+                    Claim c = list.Find(p => p.Value.Contains("S-1-5-32-544"));
+                    if (c != null)
+                        return true;
+                }
+                return false;
+            }
+        }
+
+        public PassItVPN()
+        {
+            if (IsAdmin == true)
+            {
+                InitializeComponent();
+            }
+            else if(IsAdmin == false)
+            {
+                MessageBox.Show("Start PassItVPN as an administrator!");
+                Application.Exit();
+            }
+        }
+
+        private void connectButton(object sender, EventArgs e)
+        {
+            if (ConnectButton.Text == "CONNECT")
+            {
+                Process process = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.CreateNoWindow = true; //Hide openVPN Console
+                startInfo.FileName = @"C:\Program Files\OpenVPN\bin\openvpn.exe"; //OPENVPN Path
+                startInfo.Arguments = "--config serveropenvpn.ovpn"; //OpenVPN cert
+                //startInfo.Verb = "runas"; //Run As Admin
+                process.StartInfo = startInfo;
+                process.Start();
+                MessageBox.Show("Succesfully connected to your own PassItVPN Server!.");
+                progressBar1.Value = 100;
+
+                ConnectButton.Text = "DISCONNECT";
+            }
+            else
+            {
+                Disconnect();
+                ConnectButton.Text = "CONNECT";
+            }
+        }
+
+        private void Disconnect()
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "taskkill",
+                Arguments = $"/f /im openvpn.exe",
+                CreateNoWindow = true,
+                Verb = "runas",
+                UseShellExecute = false
+            }).WaitForExit();
+            progressBar1.Value = 0;
+        }
+
+        private void LogoClick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
